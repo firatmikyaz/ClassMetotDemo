89f2f5fd12d6179d0d10ab2129e000fe909a90a5
firatmikyaz <66138781+firatmikyaz@users.noreply.github.com>
2021-01-25 23:30:26 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " - " + musteri.Name + " " +musteri.SurName + "\n" + musteri.DateBirthDay);
+            Console.WriteLine(musteri.Id + " Id'li Müşteri Eklendi.");
+        }
+        public void List(Musteri[] musteriler) 
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id + " " + musteri.Name + " " +musteri.SurName + " "+ musteri.DateBirthDay);
+                Console.WriteLine("Müşteriler Listelendi.");
+            }
+        }
+        public void Delete(Musteri musteri) 
+        {
+            Console.WriteLine(musteri.Id + " " + musteri.Name + " " + musteri.SurName + " " + musteri.DateBirthDay);
+            Console.WriteLine("Müşteriler Silindi.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Name = "Fırat";
+            musteri1.SurName = "Mikyaz";
+            musteri1.DateBirthDay = "01.08.1991";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Name = "Mustafa";
+            musteri2.SurName = "Akçakaya";
+            musteri2.DateBirthDay = "05.11.1995";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+            Console.WriteLine("*********************");
+            musteriManager.List(musteriler);
+            Console.WriteLine("*********************");
+            musteriManager.Delete(musteri1);
+            musteriManager.Delete(musteri2);
+        }
+    }
+}
